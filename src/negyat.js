c0fa11cd40d1ea7de493fe1f07e5401e83b1d2d8
Rátkay Dániel <siege.team22@gmail.com>
2023-04-20 08:59:02 +0200

    negyat.js név +

--- src/negyat.js
+++ src/negyat.js
@@ -3,3 +3,4 @@ function calculateDiagonal() {
     let diagonal = Math.sqrt(2 * side * side);
     document.getElementById("result").innerHTML = "A négyzet átlója: " + diagonal.toFixed(2);
 }
+/* Rátkay Dániel | Szoft, I-2/N | 2023.04.20*/
